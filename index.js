d60de996306e986d659763393a0e309252c2d49a
Wareezy <warrenjaftha16@gmail.com>
2023-11-20 09:51:21 +0200

    warren Jaftha

---
+++ index.js
@@ -0,0 +1,36 @@
+//- Create a function called "greet" that accepts a single parameter.Log the parameter along with a "Hello".
+function greet(name)
+{
+
+     console.log("Hello" + name)
+ }
+ greet('Warren');
+
+
+
+
+//- Create a function called multiply. It should accept two parameters and log the multiplication of the two given parameters.
+ function multiply(first, second)
+ {
+ console.log(first *second)
+
+}
+multiply(2,4);
+
+
+
+//- Create a function called tableOfFive which accepts a single parameter. Return the parameter multiplied by five to a variable, and log this variable
+function tableOfFive(number)
+{
+return number *5
+}
+tableOfFive(5);
+let number=tableOfFive(5);
+console.log(number)
+
+
+//-Please take note of the differences between log and return
+
+//console.log is a function that is mainly used to log passed in arguments to the console. simply put it is simply used to get the out put of a statement
+//however return is a keyword which is used to end a function execution. and in most cases is used to get the outcome of something which is usually the function
+
